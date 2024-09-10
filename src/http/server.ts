dfee5d083e7a826353c50e56d788317397a5cc3d
Victtor777 <victtor.eng.software@gmail.com>
2024-09-10 17:42:59 -0300

    Criando a função de criar metas

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,37 @@
+import { createGoal } from "@/functions/create-goal"
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod"
 import fastify from "fastify"
+import z from "zod"
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  "/goals",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
